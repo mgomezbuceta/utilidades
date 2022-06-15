5b0c7a1824fa7f983af4c75b3e0b77fdc720e2c4
Marcos Gómez Buceta <mgomezbuceta@gmail.com>
2022-06-15 12:17:21 +0200

    mysql-cambiar-collate.sql
    Consulta de base de datos que genera los ALTER TABLE, necesarios para cambiar la codificación de las tablas.

---
+++ mysql-cambiar-collate.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT(
+'ALTER TABLE ',  table_name, ' CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci;  ', 
+'ALTER TABLE ',  table_name, ' CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci;  ')
+FROM information_schema.TABLES AS T, information_schema.`COLLATION_CHARACTER_SET_APPLICABILITY` AS C
+WHERE C.collation_name = T.table_collation
+AND T.table_schema = 'bng_demo';
