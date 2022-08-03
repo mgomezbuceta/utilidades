27005b607c49ed4b47ce3e8ea1bac161a9595235
Marcos Gómez Buceta <mgomezbuceta@gmail.com>
2022-08-03 13:02:56 +0200

    Update mysql-cambiar-collate.sql

--- mysql-cambiar-collate.sql
+++ mysql-cambiar-collate.sql
@@ -3,4 +3,4 @@ SELECT CONCAT(
 'ALTER TABLE ',  table_name, ' CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci;  ')
 FROM information_schema.TABLES AS T, information_schema.`COLLATION_CHARACTER_SET_APPLICABILITY` AS C
 WHERE C.collation_name = T.table_collation
-AND T.table_schema = 'bng_demo';
+AND T.table_schema = 'bbdd';
